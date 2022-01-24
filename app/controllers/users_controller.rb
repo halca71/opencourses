86a42ce22803de1fd15ad98d9d1c503b60f4bec6
haruka.yoshida <mindmapping71@gmail.com>
2022-01-24 09:58:54 +0000

    add correct_favorite

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:likes]
+  before_action :correct_favorite, only: [:likes]
 
   def new
     @user = User.new
@@ -29,6 +30,13 @@ class UsersController < ApplicationController
   
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
+  def correct_favorite
+    @favorite = current_user.favorites.find_by(id: params[:id])
+    unless @favorite
+      redirect_to root_path
+    end
   end
   
 end
