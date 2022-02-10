01e91ad41eb49aa52b1c993544bc3faa2d694d2a
haruka.yoshida <mindmapping71@gmail.com>
2022-02-10 03:18:50 +0000

    edit favorite

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:likes]
-  before_action :correct_favorite, only: [:likes]
+  before_action :correct_user, only: [:likes]
 
   def new
     @user = User.new
@@ -22,7 +22,8 @@ class UsersController < ApplicationController
   end
   
   def likes
-    @user = User.find(params[:id])
+    @user = User.find_by(id: params[:id])
+    @likes = Favorite.where(user_id: @user.id)
     @pagy, @likes = pagy(@user.likes.order(id: :desc))
   end
 
@@ -32,9 +33,9 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
   
-  def correct_favorite
-    @favorite = current_user.favorites.find_by(id: params[:id])
-    unless @favorite
+  def correct_user
+    @user = User.find_by(id: params[:id])
+    unless @user == current_user
       redirect_to root_path
     end
   end
